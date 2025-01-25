a1c6695652e987c468d32107956cd8d47b3437a2
小夏 <58629523+dualseason@users.noreply.github.com>
2025-01-26 00:46:24 +0800

    Delete JeezFoundation.Algorithm_Testing directory

--- JeezFoundation.Algorithm_Testing/_usings.cs
+++
@@ -1,8 +0,0 @@
-﻿global using System;
-global using System.Linq;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using JeezFoundation.Algorithm;
-global using JeezFoundation.Algorithm.DataStructures;
-global using JeezFoundation.Algorithm.Mathematics;
-global using JeezFoundation.Algorithm.Measurements;
-global using static JeezFoundation.Algorithm.Statics;
